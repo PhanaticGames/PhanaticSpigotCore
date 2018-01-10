cca9bd6ca3c02b5c760595b0564fd0a5595d56a3
Matthew <mattparksjr@gmail.com>
2018-01-09 19:06:37 -0500

    Added caht color formatting

---
+++ src/main/java/code/matthew/psc/listener/AyncChatEvent.java
@@ -0,0 +1,22 @@
+package code.matthew.psc.listener;
+
+import code.matthew.psc.utils.strings.ColorUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class AyncChatEvent implements Listener {
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        Player p = e.getPlayer();
+
+        if (e.getMessage().contains("&")) {
+            if (p.hasPermission("psc.chatcolor")) {
+                String newMsg = ColorUtil.colorStr(e.getMessage());
+                e.setMessage(newMsg);
+            }
+        }
+    }
+}
