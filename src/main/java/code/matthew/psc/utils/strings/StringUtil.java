4e39d0db86ed0d1117af9184fef6448bad1819b9
Matthew <mattparksjr@gmail.com>
2018-01-16 16:06:27 -0500

    Added some docs to StringUtil

--- src/main/java/code/matthew/psc/utils/strings/StringUtil.java
+++ src/main/java/code/matthew/psc/utils/strings/StringUtil.java
@@ -1,7 +1,16 @@
 package code.matthew.psc.utils.strings;
 
+/**
+ * A utility  class for strings
+ */
 public class StringUtil {
 
+    /**
+     * Remove the last character from a strong
+     *
+     * @param str The string to edit
+     * @return The string provided, minus the last character
+     */
     public static String removeLastChar(String str) {
         return str.substring(0, str.length() - 1);
     }
