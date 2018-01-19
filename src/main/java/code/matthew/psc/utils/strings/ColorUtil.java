1852380f78e509abdc2712b0c72ac092f26be511
Matthew <mattparksjr@gmail.com>
2018-01-19 13:07:59 -0500

    Fixed java doc

--- src/main/java/code/matthew/psc/utils/strings/ColorUtil.java
+++ src/main/java/code/matthew/psc/utils/strings/ColorUtil.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class ColorUtil {
 
     /**
-     * Color a string, Uses & for colors
+     * Color a string.
      *
      * @param str The string to color
      * @return The string, colored
