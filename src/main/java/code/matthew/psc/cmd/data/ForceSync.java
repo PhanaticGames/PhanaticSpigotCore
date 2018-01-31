c7d1f0628e5716fb46347ac7fd9fcbe26cd81370
Matthew <mattparksjr@gmail.com>
2018-01-31 17:18:33 -0500

    Forsync now includes mutes

--- src/main/java/code/matthew/psc/cmd/data/ForceSync.java
+++ src/main/java/code/matthew/psc/cmd/data/ForceSync.java
@@ -3,6 +3,7 @@ package code.matthew.psc.cmd.data;
 import code.matthew.psc.PSC;
 import code.matthew.psc.api.command.ICommand;
 import code.matthew.psc.runnable.AsyncBans;
+import code.matthew.psc.runnable.AsyncMutes;
 import code.matthew.psc.utils.data.ConfigCache;
 import org.bukkit.command.CommandSender;
 
@@ -14,8 +15,8 @@ public class ForceSync extends ICommand {
 
     @Override
     public boolean finalExe(CommandSender sender, String[] args) {
-        // TODO ADD ALL TYPES OF DATA
         PSC.getInstance().getServer().getScheduler().runTask(PSC.getInstance(), new AsyncBans());
+        PSC.getInstance().getServer().getScheduler().runTask(PSC.getInstance(), new AsyncMutes());
         sender.sendMessage(ConfigCache.getMsg("forcedASync"));
         return true;
     }
